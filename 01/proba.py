0910ea559d187b093357d0e187cbc1a2da890552
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-09-15 16:08:41 +0200

    .

---
+++ 01/proba.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    a = "abcd"
+    b = "xy"
+
+    lengthA = len(a)
+    lengthB = len(b)
+    #if (lengthA % 2 == 0) and (lengthB % 2 == 0):
+    #    s = a[:lengthA / 2] + b[:lengthB / 2] + a[lengthA / 2:] + b[lengthB:]
+
+    print(a[:lengthA/2])
+
+if __name__ == "__main__":
+        main()
