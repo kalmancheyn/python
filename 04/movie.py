22d54ad709be34f397051efc54c77b9030dac83e
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-09-28 16:07:03 +0200

    .

---
+++ 04/movie.py
@@ -0,0 +1,14 @@
+
+
+def get_movie_info():
+    return ("Total Recall", 1990, 7.5)
+
+
+def main():
+    t = get_movie_info()
+    cim, ev, pont = get_movie_info()
+    print(cim, ev, pont)
+
+
+if __name__ == '__main__':
+    main()
