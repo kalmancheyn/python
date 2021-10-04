eab9dc764d8d736e587f82b686c6396be8735ebb
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-10-04 14:01:22 +0200

    .

--- hf4/hangrend.py
+++ hf4/hangrend.py
@@ -1,18 +1,28 @@
 
-
-mely = "a,á,o,ó,u,ú"
-magas = "e,é,i,í,ö,ü,ű"
+MELY_MGHK = 'aáoóuú'
+MAGAS_MGHK = 'eéiíöőüű'
 
 
 def main():
     s = input("Kérem adjon meg egy szót: ")
-    counter = None
+    mely = False
+    magas = False
+    for char in s:
+        for i in MELY_MGHK:
+            if i == char:
+                mely = True
+        for j in MAGAS_MGHK:
+            if j == char:
+                magas = True
 
-    
-
-
-
-
+    if magas and mely:
+        print("vegyes")
+    elif magas:
+        print("magas")
+    elif mely:
+        print("mély")
+    else:
+        print("semmilyen")
 
 
 if __name__ == "__main__":
---
+++ hf4/listcomprehension.py
@@ -0,0 +1,40 @@
+
+
+def main():
+    # nums = [1, 2, 3, 4]
+    # squares = [n * n for n in nums]
+    # print(squares)
+    #1
+    # inp = ['auto', 'villamos', 'metro']
+    # result = [s.upper() +"!" for s in inp]
+    # print(result)
+    #2
+    # inp = ['aladar', 'bela', 'cecil']
+    # result = [s.capitalize() for s in inp]
+    # print(result)
+    #3
+    # result = [0 for value in range(10)]
+    # print(result)
+    #4
+    # inp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # result = [n * 2 for n in inp]
+    # print(result)
+    #5
+    # inp = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    # result = [int(n) for n in inp]
+    # print(result)
+    #6
+    # inp = "1234567"
+    # result = [int(n) for n in inp]
+    # print(result)
+    #7
+    #s = 'The quick brown fox jumps over the lazy dog'
+    #result = [len(value) for value in s.split()]
+    #print(result)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
