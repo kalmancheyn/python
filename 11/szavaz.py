f49ee9e6b43b3dc144e8afbab14d14eb265ec622
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-12-06 13:23:44 +0100

    .

--- 11/szavaz.py
+++ 11/szavaz.py
@@ -1,13 +1,22 @@
 import requests
-from time import sleep
+import time
+import random as r
+
+
+def sleep(fix=5, plus=5):
+    """Sleep for some randomized time.
+
+    Time will be a real value between fix and fix+plus."""
+    sec = fix + r.uniform(0, plus)
+    time.sleep(sec)
+    return sec
 
 
 def main():
     url = "https://divany.hu/offline/2016/03/07/dontsuk_el_meno_vagy_ciki_az_ovtaska/?p=&meno=1&posztbol=1"
     for i in range(10):
         r = requests.get(url)
-        html = r.text
-        sleep(1)
+        sleep()
         print('.', end="", flush=True)
     print()
 
